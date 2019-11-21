440053e4dea1a11a0f2d0dd4e9fce4ef3b6d78b8
Nathan Farlow <natethegreatest11@gmail.com>
2019-11-21 13:48:29 -0600

    Add constant matrix functions

--- src/matrixexecutor.h
+++ src/matrixexecutor.h
@@ -5,9 +5,15 @@ class Matrix;
 
 template <typename T>
 class MatrixExecutor {
+    virtual Matrix<T> add(const Matrix<T> &m, const T &val);
     virtual Matrix<T> add(const Matrix<T> &m1, const Matrix<T> &m2) = 0;
+
+    virtual Matrix<T> sub(const Matrix<T> &m, const T &val);
     virtual Matrix<T> sub(const Matrix<T> &m1, const Matrix<T> &m2) = 0;
+
+    virtual Matrix<T> mul(const Matrix<T> &m, const T &val);
     virtual Matrix<T> mul(const Matrix<T> &m1, const Matrix<T> &m2) = 0;
+    
     virtual Matrix<T> tensor(const Matrix<T> &m1, const Matrix<T> &m2) = 0;
 };
 
--- src/naiveexecutor.cpp
+++ src/naiveexecutor.cpp
@@ -1,11 +1,23 @@
 #include "matrixexecutor.h"
 
+Matrix<T> NaiveExecutor::add(const Matrix<T> &m, const T &val) {
+
+}
+
 Matrix<T> NaiveExecutor::add(const Matrix<T> &m1, const Matrix<T> &m2) {
     
 }
 
+Matrix<T> NaiveExecutor::sub(const Matrix<T> &m, const T &val) {
+
+}
+
 Matrix<T> NaiveExecutor::sub(const Matrix<T> &m1, const Matrix<T> &m2) {
     
+}
+
+Matrix<T> NaiveExecutor::mul(const Matrix<T> &m, const T &val) {
+
 }
 
 Matrix<T> NaiveExecutor::mul(const Matrix<T> &m1, const Matrix<T> &m2) {
