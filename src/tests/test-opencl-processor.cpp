27685dc0fe9add278fa6a36ae07cb1c16552b2a9
Nathan Farlow <natethegreatest11@gmail.com>
2019-12-04 02:05:33 -0600

    Add opencl tests

--- src/tests/test-opencl-processor.cpp
+++ src/tests/test-opencl-processor.cpp
@@ -2,9 +2,21 @@
 
 #include "../openclprocessor.h"
 
+using namespace quantum;
+
 TEST_CASE("Multiplication") {
 
-	REQUIRE_NOTHROW(OpenCLProcessor("src/openclprocessor.kernel"));
+	auto opencl_processor = std::make_shared<OpenCLProcessor>("src/openclprocessor.kernel");
+
+	QState s1({{complex(1, 3), complex(2, 2)}, {complex(3), complex(4, 1)}});
+	QState s2(s1);
+
+	QState expected = s1 * s2;
+
+	s1.set_processor(opencl_processor);
+	QState received = s1 * s2;
+
+	REQUIRE(expected.ToString() == received.ToString());
 
 	SECTION("Too small") {
 
